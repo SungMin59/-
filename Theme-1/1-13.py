0caf030d2cdbfe7688ada770b2804987ed511b0c
Lim <bigmanmin1@naver.com>
2022-04-30 23:26:06 +0900

    update 1-13.py

--- Theme-1/1-13.py
+++ Theme-1/1-13.py
@@ -1,8 +1,2 @@
 import sys
-A, B = map(int, sys.stdin.readline().split())
-print(A)
-print(B)
-print(A*B[0])
-print(A*B[1])
-print(A*B[2])
-print(A*B)
+inp = [int(x) for x in  sys.stdin.readline().split()]
