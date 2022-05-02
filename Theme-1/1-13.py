fbc34e7e86e9a2e04f416a565656828e6c7fad58
Lim <bigmanmin1@naver.com>
2022-05-02 11:16:50 +0900

    update 1-13.py

--- Theme-1/1-13.py
+++ Theme-1/1-13.py
@@ -1,6 +1,6 @@
 import sys
 A, B = sys.stdin.readline().split()
-print(int(A)*int(B[0]))
-print(int(A)*int(B[1]))
 print(int(A)*int(B[2]))
+print(int(A)*int(B[1]))
+print(int(A)*int(B[0]))
 print(int(A)*int(B))
