7388e40c4c38cf656d3cbfb62d027901496ea463
Lim <bigmanmin1@naver.com>
2022-05-03 14:50:17 +0900

    update 2-7.py

--- Theme-2/2-7.py
+++ Theme-2/2-7.py
@@ -3,7 +3,9 @@ import sys
 A,B,C=map(int,sys.stdin.readline().split()))
 if A==B==C:
 	print(10000+A*1000)
-if A==B or B==C:
-	print(B)
-if A==C:
-	print(*max(d))
+elif A==B or B==C:
+	print(1000+B*100)
+elif A=C:
+	print(1000+A*100)
+else:
+	print(max(A,B,C)*100)
