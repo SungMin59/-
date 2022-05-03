aba9794fde23b91e62e4b3a6e16d3f5d5384830a
Lim <bigmanmin1@naver.com>
2022-05-03 10:49:49 +0900

    update 2-1.py

--- Theme-2/2-1.py
+++ Theme-2/2-1.py
@@ -1,11 +1,7 @@
-score = int(input())
-if 100>= score >= 90:
+A,B = map(int,sys.stdin.readline().split())
+if 100>= score:
     print('A')
 elif 90 > score >= 80:
     print('B')
-elif 80 > score >= 70:
-    print('C')
-elif 70 > score >= 60:
-    print('D')
 else:
     print('F')
