b4707bf361cb6047a52f56c23d40bebb82de1e94
Lim <bigmanmin1@naver.com>
2022-05-03 10:50:31 +0900

    update 2-1.py

--- Theme-2/2-1.py
+++ Theme-2/2-1.py
@@ -1,7 +1,7 @@
 A,B = map(int,sys.stdin.readline().split())
-if 100>= score:
-    print('A')
-elif 90 > score >= 80:
-    print('B')
+if A<B:
+    print('<')
+elif A>B:
+    print('>')
 else:
-    print('F')
+    print('=')
