d361095470255eda72407fff0b5643c345415d9c
Lim <bigmanmin1@naver.com>
2022-05-03 12:55:00 +0900

    update 2-6.py

---
+++ Theme-2/2-6.py
@@ -0,0 +1,16 @@
+import sys
+A,B=map(int,sys.stdin.readline().split())
+C=int(input())
+if B+C<60:
+	B+=C
+	if A==24:
+		A=0
+	else:
+		A+=1
+else:
+	B-=60-C
+	if A==24:
+		A=0
+	else:
+		A+=1
+print(A,B)
