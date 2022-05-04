ced0336b1ae15ac0087f0b2a96c3a5021fedb7da
Lim <68288682+SungMin59@users.noreply.github.com>
2022-05-04 12:51:54 +0000

    update 3-12.py

--- Theme-1/1-10.py
+++ Theme-1/1-10.py
@@ -1,1 +1,2 @@
 print(f'{input()}??!')
+# print(input(),end='??!')
--- Theme-3/3-11.py
+++ Theme-3/3-11.py
@@ -1,8 +1,17 @@
+# Solve 1
 import sys
 N,X = map(int,sys.stdin.readline().split())
 inp = [x for x in map(int,sys.stdin.readline().split())]
-arr = []
-for i in inp:
-	if X<i:
-		arr.append(i)
-print(arr)
+
+for i in range(N):
+	if X>inp[i]:
+		print(inp[i], end=" ")
+
+# Solve 2
+# import sys
+# N,X = map(int,sys.stdin.readline().split())
+# inp = list(map(int,sys.stdin.readline().split()))
+
+# for i in inp:
+# 	if X>i:
+# 		print(i, end=" ")
