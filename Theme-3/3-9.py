1e76d19f5253d14189cfacfd8b96f60a24606ec5
Lim <bigmanmin1@naver.com>
2022-05-04 11:15:57 +0900

    update 3-9.py

--- Theme-3/3-9.py
+++ Theme-3/3-9.py
@@ -1,7 +1,3 @@
-import sys
 N=int(input())
-inp = [x for x in map(int,sys.stdin.readline().rstrip().split())]
-max = 0
-for i in range(inp):
-	if inp[i]>inp[i+1]:
-		
+for i in range(N):
+	print((i+1)*'*')
