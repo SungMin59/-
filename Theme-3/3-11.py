7d42f08cb6d882791c7cbeac2d3ed97a7eac28df
Lim <bigmanmin1@naver.com>
2022-05-04 19:17:02 +0900

    update 3-11.py

--- Theme-3/3-11.py
+++ Theme-3/3-11.py
@@ -1,6 +1,6 @@
 import sys
-N,X = map(int,sys.stdin.readline().rstrip().split())
-inp = [x for x in map(int,sys.stdin.readline().rstrip().split())]
+N,X = map(int,sys.stdin.readline().split())
+inp = [x for x in map(int,sys.stdin.readline().split())]
 for i in inp:
 	if X>i:
 		
